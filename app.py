9c132ad5ef00c860670145bfee66d6c763ef8e69
github ParkBig <github enfantgu@gmail.com>
2022-09-25 19:40:54 +0900

    나의 첫 팀플 토이프로젝트

---
+++ app.py
@@ -0,0 +1,234 @@
+from datetime import datetime, timedelta
+from time import strftime
+import hashlib
+import jwt
+import requests
+from bs4 import BeautifulSoup
+from werkzeug.utils import secure_filename
+from bson.objectid import ObjectId
+
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+app = Flask(__name__)
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://ParkBigKing:anjfqhk@cluster0.cfmmcms.mongodb.net/?retryWrites=true&w=majority')
+db = client.gamestorytest
+
+
+SECRET_KEY = 'game is my life'
+
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+##로그인 코드
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    #비밀번호 암호화(hash)
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    #회원가입 정보 중 암호화 비밀번호와 로그인 암호화 비밀번호가 일치 하는게 있는지 확인한다.
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/content', methods=['GET'])
+def show_diary():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username_receive = request.args.get("username_give")
+        if username_receive=="":
+            posts = list(db.posts.find({}).sort("date", -1).limit(20))
+        else:
+            posts = list(db.posts.find({"username":username_receive}).sort("date", -1).limit(20))
+        for post in posts:
+            post["_id"] = str(post["_id"])
+            post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "heart"})
+            post["heart_by_me"] = bool(db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
+        return jsonify({"posts":posts, "name":payload['id']})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+@app.route('/delete', methods=['POST'])
+def delete_content():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    _id = request.form['id_give']
+    user_info = db.posts.delete_one({'_id':ObjectId(_id)})
+    print(user_info)
+    return jsonify({'result': 'success','msg':'삭제 완료!'})
+    # 좋아요 수 변경
+    # user_info = db.posts.find_one({"_id": payload["id"]})
+
+
+@app.route('/content', methods=['POST'])
+def save_content():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user_info = db.users.find_one({"username": payload["id"]})
+    today = datetime.now()
+    now = today.strftime('%y-%m-%d-%H-%M-%S')
+    date = today.strftime('%y년 %m월') 
+    content_receive = request.form['content_give']
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    # 좋아요 수 변경
+    user_info = db.users.find_one({"username": payload["id"]})
+    try :
+        file = request.files["file_give"]
+        extension = file.filename.split('.')[-1]
+        filename = f'file-{now}'
+        save_to = f'static/img/{filename}.{extension}'
+        file.save(save_to)
+        doc ={
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            'content':content_receive,
+            'file':f'{filename}.{extension}',
+            'date':datetime.now().isoformat(),
+            "username": user_info["username"]
+        }
+        db.posts.insert_one(doc)
+        return jsonify({'msg': '저장완룡'})
+    except :
+        doc ={
+            "username": user_info["username"],
+            "profile_name": user_info["profile_name"],
+            "profile_pic_real": user_info["profile_pic_real"],
+            'content':content_receive,
+            'file':'none',
+            'date':datetime.now().isoformat(),
+            "username": user_info["username"]
+        }
+        db.posts.insert_one(doc)
+        return jsonify({'msg': '저장완룡'})
+
+
+# 로그인파트##################################################################### 추가내용
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('signup.html', msg=msg)
+
+
+##회원가입 코드
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""   
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+# 클라이언트에서 통과 한 값을 db에 있는지 없는지 체크해서 알려준다.
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+# 마이페이지파트##################################################################### 추가내용
+
+@app.route('/user/<username>')
+def user(username):
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/update_profile', methods=['POST'])
+def save_img():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    username = payload["id"]
+    today = datetime.now()
+    now = today.strftime('%y-%m-%d-%H-%M-%S')
+    name_receive = request.form["name_give"]
+    about_receive = request.form["about_give"]
+    new_doc = {
+            "profile_name": name_receive,
+            "profile_info": about_receive
+        }
+    try:
+        file = request.files["file_give"]
+        filename = secure_filename(file.filename)###
+        extension = file.filename.split('.')[-1]
+        file_path = f"profile_pics/{username}.{extension}"###
+        file.save("./static/"+file_path)##
+        new_doc["profile_pic"] = filename
+        new_doc["profile_pic_real"] = f"{username}.{extension}"
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+    except:
+        db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+        return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
+
+@app.route('/update_like', methods=['POST'])
+def update_like():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        # 좋아요 수 변경
+        user_info = db.users.find_one({"username": payload["id"]})
+        post_id_receive = request.form["post_id_give"]
+        type_receive = request.form["type_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "post_id": post_id_receive,
+            "username": user_info["username"],
+            "type": type_receive
+        }
+        if action_receive == "like":
+
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/myjs.js
@@ -0,0 +1,231 @@
+$(document).ready(function () {
+    $("#file").on('change',function(){
+        var fileName = $("#file").val();
+        $(".upload-name").val(fileName);
+    });
+});
+
+function listing(username) {
+    if (username==undefined) {
+        username=""
+    }
+    $.ajax({
+        type: "GET",
+        url: `/content?username_give=${username}`,
+        data: {},
+        success: function (response) {
+            let content = response['posts'];
+            let my_name = response['name']
+            console.log(my_name)
+            for (let i = 0; i<content.length; i++) {
+                let post = content[i]
+                console.log(post)
+                let class_heart = ""
+                let time_post = new Date(post["date"])
+                let time_before = time2str(time_post)
+                let delete_button = "none"
+                if (post["heart_by_me"]) {
+                    class_heart = "fa-heart"
+                } else {
+                    class_heart = "fa-heart-o"
+                }
+                if (post["username"]==my_name) {
+                    delete_button = ""
+                } else {
+                    delete_button = "none"
+                }
+                let count_heart = post['count_heart']
+                if (content[i]['file'] === "none") {
+                    let value = `<section class="section">
+                                    <div id="post-box" class="container">
+                                        <div class="box">
+                                            <article class="media">
+                                                <div class="media-left">
+                                                    <a class="image is-64x64" href="/user/${content[i]['username']}">
+                                                        <img class="is-rounded" src="/static/profile_pics/${post['profile_pic_real']}" alt="">
+                                                    </a>
+                                                </div>
+                                                <div class="media-content">
+                                                    <div class="content">
+                                                        <p>
+                                                            <strong>${post['username']}</strong><small> ${ time_before }</small><button onclick="delete_content('${post["_id"]}')" style="position: absolute;top:5px; right: 5px; display: ${delete_button} " class="button level-item has-text-centered is-sparta is-outlined">delete</button>
+                                                            <br>
+                                                            ${content[i]['content']}
+                                                        </p>
+                                                    </div>
+                                                    <nav class="level is-mobile">
+                                                        <div class="level-left" id="${post["_id"]}">
+                                                            <a class="level-item is-sparta" aria-label="heart" onclick="toggle_like('${post['_id']}', 'heart')">
+                                                                <span class="icon is-small"><i class="fa ${class_heart}" aria-hidden="true"></i></span>&nbsp;<span class="like-num">${num2str(count_heart)}</span>
+                                                            </a>
+                                                        </div>
+                                                    </nav>
+                                                </div>
+                                            </article>
+                                        </div>
+                                    </div>
+                                </section>`;
+                    $(".has-navbar-fixed-top").append(value)
+                } else {
+                    let value = `<section class="section">
+                                    <div id="post-box" class="container">
+                                        <div class="wrap">
+                                            <div class="box child1" style="margin-bottom: 0px; height: 400px; width: 346px; margin-right: 10px; text-align: center;">
+                                                <img src="../static/img/${content[i]['file']}" style="max-height: 360px; max-width: 310px; ">
+                                            </div>
+                                            <div class="box child2" style="height: 400px; width: 346px;">
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <a class="image is-64x64" href="/user/${content[i]['username']}">
+                                                            <img class="is-rounded" src="/static/profile_pics/${post['profile_pic_real']}" alt="">
+                                                        </a>
+                                                    </div>
+                                                    <div class="media-content">
+                                                        <div class="content">
+                                                            <p>
+                                                                <strong>${post['username']}</strong> <small> ${ time_before }</small><button onclick="delete_content('${post["_id"]}')" style="position: absolute;top:5px; right: 5px; display: ${delete_button} " class="button level-item has-text-centered is-sparta is-outlined">delete</button>
+                                                            </p>
+                                                        </div>
+                                                        <nav class="level is-mobile">
+                                                            <div class="level-left" id="${post["_id"]}">
+                                                                <a class="level-item is-sparta" aria-label="heart" onclick="toggle_like('${post['_id']}', 'heart')">
+                                                                    <span class="icon is-small"><i class="fa ${class_heart}" aria-hidden="true"></i></span>&nbsp;<span class="like-num">${num2str(count_heart)}</span>
+                                                                </a>
+                                                            </div>
+                                                        </nav>
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <p>${content[i]['content']}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>`;
+                        $(".has-navbar-fixed-top").append(value)
+                }
+            }
+        }
+    })
+}
+
+function delete_content(_id) {
+    console.log(_id)
+    let form_data = new FormData();
+    form_data.append("id_give", _id);
+    $.ajax({
+        type: "POST",
+        url: "/delete",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    });
+}
+
+function post() {
+    let content = $("#textarea-post").val()
+    let file = $('#file')[0].files[0];
+    let form_data = new FormData();
+
+    if (file === "undefined") {
+        form_data.append("content_give", content);
+    } else {
+        form_data.append("content_give", content);
+        form_data.append("file_give", file);
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/content",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    });
+}
+
+ function sign_out() {
+                $.removeCookie('mytoken', {path: '/'});
+                alert('로그아웃!')
+                window.location.href = "/login"
+            }
+
+
+function toggle_like(post_id, type) {
+    console.log(post_id, type)
+    let $a_like = $(`#${post_id} a[aria-label='heart']`)
+    let $i_like = $a_like.find("i")
+    if ($i_like.hasClass("fa-heart")) {
+        $.ajax({
+            type: "POST",
+            url: "/update_like",
+            data: {
+                post_id_give: post_id,
+                type_give: type,
+                action_give: "unlike"
+            },
+            success: function (response) {
+                console.log("unlike")
+                $i_like.addClass("fa-heart-o").removeClass("fa-heart")
+                $a_like.find("span.like-num").text(num2str(response["count"]))
+            }
+        })
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "/update_like",
+            data: {
+                post_id_give: post_id,
+                type_give: type,
+                action_give: "like"
+            },
+            success: function (response) {
+                console.log("like")
+                $i_like.addClass("fa-heart").removeClass("fa-heart-o")
+                $a_like.find("span.like-num").text(num2str(response["count"]))
+            }
+        })
+
+    }
+}
+
+function num2str(count) {
+    if (count > 10000) {
+        return parseInt(count / 1000) + "k"
+    }
+    if (count > 500) {
+        return parseInt(count / 100) / 10 + "k"
+    }
+    if (count == 0) {
+        return ""
+    }
+    return count
+}
+
+function time2str(date) {
+    let today = new Date()
+    let time = (today - date) / 1000 / 60  // 분
+
+    if (time < 60) {
+        return parseInt(time) + "분 전"
+    }
+    time = time / 60  // 시간
+    if (time < 24) {
+        return parseInt(time) + "시간 전"
+    }
+    time = time / 24
+    if (time < 7) {
+        return parseInt(time) + "일 전"
+    }
+    return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
+}
+
